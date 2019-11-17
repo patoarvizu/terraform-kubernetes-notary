59a2589f870dd8cdac57a8f80a74113f248dbb3d
patoarvizu <patoarvizu@gmail.com>
2019-11-17 15:03:54 -0500

    Split 'kubernetes_persistent_volume_claim' resources out into a separate file

--- main.tf
+++ main.tf
@@ -1,23 +1,3 @@
-resource "kubernetes_persistent_volume_claim" "notary_data" {
-  for_each = var.deploy_persistence == true ? {create: true} : {}
-  metadata {
-    name = "notary-data"
-    namespace = var.namespace
-  }
-  spec {
-    storage_class_name = var.storage_class_name
-    access_modes = [
-      "ReadWriteOnce"
-    ]
-    resources {
-      requests = {
-        storage = var.storage_size
-      }
-    }
-  }
-  wait_until_bound = false
-}
-
 resource "kubernetes_deployment" "notary_db" {
   for_each = var.deploy_persistence == true ? {create: true} : {}
   metadata {
